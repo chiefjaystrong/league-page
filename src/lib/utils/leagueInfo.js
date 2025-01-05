c1142b81f23e0e70839c3a3a86783187c1152d1b
chiefjaystrong <jdstrong1002@gmail.com>
2025-01-04 19:53:14 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Strong Burber League<p>
+  <p>Bible League<p>
 `;
 
 /*   STEP 3   */
